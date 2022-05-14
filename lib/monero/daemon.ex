053a8ad54443602cc78853768fa7ca8c0cfc0649
Nurviro <nurviro@protonmail.com>
2022-05-14 15:52:26 -0400

    add more rpc methods

--- lib/monero/daemon.ex
+++ lib/monero/daemon.ex
@@ -4,6 +4,9 @@ defmodule Monero.Daemon do
   https://lessless.github.io temporarly.
   """
 
+  ## JSON RPC Methods
+  ######################
+
   @doc "Look up how many blocks are in the longest chain known to the node."
   @spec get_block_count() :: Monero.Operation.Query.t()
   def get_block_count() do
@@ -53,16 +56,84 @@ defmodule Monero.Daemon do
   def get_block_headers_range(start_height, end_height) do
     rpc_request("get_block_headers_range", %{ start_height: start_height, end_height: end_height })
   end
-  #get_block
-  #get_connections
-  #get_info
-  # hard_fork_info
-  #set_bans
-  #get_bans
-  #flush_txpool
-  #get_output_histogram
-  #get_version
-  #get_coinbase_tx_sum
+
+  @doc "Full block information can be retrieved by block height."
+  def get_block(height) when is_integer(height) do
+    rpc_request("get_block", %{ height: height })
+  end
+
+  @doc "Full block information can be retrieved by hash."
+  def get_block(hash) when is_bitstring(hash) do
+    rpc_request("get_block", %{ hash: hash })
+  end
+
+  @doc "Retrieve information about incoming and outgoing connections to your node."
+  def get_connections() do
+    rpc_request("get_connections")
+  end
+
+  @doc "Retrieve general information about the state of your node and the network."
+  def get_info() do
+    rpc_request("get_info")
+  end
+
+  @doc "Look up information regarding hard fork voting and readiness."
+  def hard_fork_info() do
+    rpc_request("hard_fork_info")
+  end
+
+  @doc "Ban another node by IP."
+  def set_bans(bans) do
+    rpc_request("set_bans", %{ bans: bans })
+  end
+
+  def ban(host, seconds) when is_bitstring(host) and is_integer(seconds) do
+    set_bans([%{ host: host, ban: true, seconds: seconds }])
+  end
+
+  def ban(ip, seconds) when is_integer(ip) and is_integer(seconds) do
+    set_bans([%{ ip: ip, ban: true, seconds: seconds }])
+  end
+
+  @doc "Get list of banned IPs."
+  def get_bans() do
+    rpc_request("get_bans")
+  end
+
+  @doc "Flush all tx ids from transaction pool"
+  def flush_txpool() do
+    rpc_request("flush_txpool")
+  end
+
+  @doc "Flush an array of txids from transaction pool."
+  def flush_txpool(txids) when is_list(txids) do
+    rpc_request("flush_txpool", %{ txids: txids })
+  end
+
+  @doc """
+  Get a histogram of output amounts. For all amounts (possibly filtered by parameters),
+  gives the number of outputs on the chain for that amount. RingCT outputs count as 0 amount.
+  """
+  def get_output_histogram(amounts, min_count, max_count, unlocked, recent_cutoff) do
+    rpc_request("get_output_histogram", %{
+      amounts: amounts,
+      min_count: min_count,
+      max_count: max_count,
+      unlocked: unlocked,
+      recent_cutoff: recent_cutoff
+    })
+  end
+
+  @doc "Get the current node version."
+  def get_version() do
+    rpc_request("get_version")
+  end
+
+
+  @doc "Get the coinbase amount and the fees amount for n last blocks starting at particular height."
+  def get_coinbase_tx_sum(height, count) do
+    rpc_request("get_coinbase_tx_sum", %{ height: height, count: count })
+  end
 
   @doc "Gives an estimation on fees per byte."
   @spec get_fee_estimate() :: Monero.Operation.Query.t()
@@ -70,8 +141,19 @@ defmodule Monero.Daemon do
     rpc_request("get_fee_estimate")
   end
 
-  #get_alternate_chains
-  #relay_tx
+  def get_fee_estimate(grace_blocks) do
+    rpc_request("get_fee_estimate", %{ grace_blocks: grace_blocks })
+  end
+
+  @doc "Display alternative chains seen by the node."
+  def get_alternate_chains() do
+    rpc_request("get_alternate_chains")
+  end
+
+  @doc "Relay a list of transaction IDs."
+  def relay_tx(txids) do
+    rpc_request("relay_tx", %{ txids: txids })
+  end
 
   @doc "Get synchronization information"
   @spec sync_info() :: Monero.Operation.Query.t()
@@ -93,8 +175,15 @@ defmodule Monero.Daemon do
     )
   end
 
+  ## Other RPC Methods
+  #######################
 
-  #/get_height
+  @doc "Get the node's current height."
+  @spec get_height() :: Monero.Operation.Query.t()
+  def get_height() do
+    request("get_height")
+  end
+
   #/get_blocks.bin
   #/get_blocks_by_height.bin
   #/get_hashes.bin
@@ -103,28 +192,6 @@ defmodule Monero.Daemon do
   #/get_transactions
   #/get_alt_blocks_hashes
   #/is_key_image_spent
-  #/send_raw_transaction
-  #/start_mining
-  #/stop_mining
-  #/mining_status
-  #/save_bc
-  #/get_peer_list
-  #/set_log_hash_rate
-  #/set_log_level
-  #/set_log_categories
-  #/get_transaction_pool
-  #/get_transaction_pool_hashes.bin
-  #/get_transaction_pool_stats
-  #/stop_daemon
-  #/get_info (not JSON)
-  #/get_limit
-  #/set_limit
-  #/out_peers
-  #/in_peers
-  #/start_save_graph
-  #/stop_save_graph
-  #/get_outs
-  #/update
 
   @doc """
   Broadcast a raw transaction to the network.
@@ -134,16 +201,35 @@ defmodule Monero.Daemon do
   """
   @spec send_raw_transaction(String.t(), bool()) :: Monero.Operation.Query.t()
   def send_raw_transaction(tx_as_hex, do_not_relay \\ false) do
-    rpc_request("send_raw_transaction",
+    request("send_raw_transaction",
       %{tx_as_hex: tx_as_hex, do_not_relay: do_not_relay}
     )
   end
 
-  @doc "Get the node's current height."
-  @spec get_height() :: Monero.Operation.Query.t()
-  def get_height() do
-    rpc_request("get_height")
+  #/start_mining
+  #/stop_mining
+  #/mining_status
+  #/save_bc
+
+  @doc "Get the known peers list."
+  def get_peer_list() do
+    request("get_peer_list")
   end
+
+  #/set_log_hash_rate
+  #/set_log_level
+  #/set_log_categories
+  #/get_transaction_pool
+  #/get_transaction_pool_hashes.bin
+  #/get_transaction_pool_stats
+  #/stop_daemon
+  #/get_limit
+  #/set_limit
+  #/out_peers
+  #/in_peers
+  #/get_outs
+  #/update
+
 
   ## Request
   ######################
--- test/lib/monero/daemon_test.exs
+++ test/lib/monero/daemon_test.exs
@@ -23,6 +23,11 @@ defmodule Monero.DaemonTest do
     assert expected == Daemon.get_block_template("asdf", 24).data
   end
 
+  test "submit_block/1" do
+    expected = %{jsonrpc: "2.0", method: "submit_block", params: ["asdf"] }
+    assert expected == Daemon.submit_block(["asdf"]).data
+  end
+
   test "get_last_block_header/0" do
     expected = %{jsonrpc: "2.0", method: "get_last_block_header", params: nil}
     assert expected == Daemon.get_last_block_header().data
@@ -35,29 +40,123 @@ defmodule Monero.DaemonTest do
     assert expected == Daemon.get_block_header_by_hash("asdf").data
   end
 
-  test "submit_block/1" do
-    expected = %{jsonrpc: "2.0", method: "submit_block", params: ["asdf"] }
-    assert expected == Daemon.submit_block(["asdf"]).data
+  test "get_block_header_by_height/1" do
+    expected = %{jsonrpc: "2.0", method: "get_block_header_by_height", params: %{
+                  height: 1
+                }}
+    assert expected == Daemon.get_block_header_by_height(1).data
   end
+
+  test "get_block_headers_range/2" do
+    expected = %{jsonrpc: "2.0", method: "get_block_headers_range", params: %{
+                  start_height: 1, end_height: 100
+                }}
+    assert expected == Daemon.get_block_headers_range(1, 100).data
+  end
+
+  test "get_block/1 height" do
+    expected = %{jsonrpc: "2.0", method: "get_block", params: %{ height: 69 }}
+    assert expected == Daemon.get_block(69).data
+  end
+
+  test "get_block/1 hash" do
+    expected = %{jsonrpc: "2.0", method: "get_block", params: %{ hash: "asdf" }}
+    assert expected == Daemon.get_block("asdf").data
+  end
+
+  test "get_connections/0" do
+    expected = %{jsonrpc: "2.0", method: "get_connections", params: nil}
+    assert expected == Daemon.get_connections().data
+  end
+
+  test "get_info/0" do
+    expected = %{jsonrpc: "2.0", method: "get_info", params: nil}
+    assert expected == Daemon.get_info().data
+  end
+
+  test "hard_fork_info/0" do
+    expected = %{jsonrpc: "2.0", method: "hard_fork_info", params: nil}
+    assert expected == Daemon.hard_fork_info().data
+  end
+
+  test "set_bans/1" do
+    bans = [
+      %{
+        host: "192.168.1.1",
+        ban: true,
+        seconds: 100000
+      }
+    ]
+    expected = %{jsonrpc: "2.0", method: "set_bans", params: %{ bans: bans }}
+    assert expected == Daemon.set_bans(bans).data
+  end
+
+  test "get_bans/0" do
+    expected = %{jsonrpc: "2.0", method: "get_bans", params: nil}
+    assert expected == Daemon.get_bans().data
+  end
+
+  #flush_txpool
+  #get_output_histogram
+  #get_version
+  #get_coinbase_tx_sum
 
   test "get_fee_estimate/0" do
     expected = %{jsonrpc: "2.0", method: "get_fee_estimate", params: nil}
     assert expected == Daemon.get_fee_estimate().data
   end
 
+  #get_alternate_chains
+  #relay_tx
+  #sync_info
+  #get_txpool_backlog
+
   test "get_height/0" do
-    expected = %{jsonrpc: "2.0", method: "get_height", params: nil}
-    assert expected == Daemon.get_height().data
+    assert "get_height" == Daemon.get_height().action
   end
+
+  #/get_blocks.bin
+  #/get_blocks_by_height.bin
+  #/get_hashes.bin
+  #/get_o_indexes.bin
+  #/get_outs.bin
+  #/get_transactions
+  #/get_alt_blocks_hashes
+  #/is_key_image_spent
 
   test "send_raw_transaction/1" do
     tx_hex = "010002028090c...81a2e3bc0039cb0a02"
 
     expected = %{
-      jsonrpc: "2.0",
-      method: "send_raw_transaction",
-      params: %{ tx_as_hex: tx_hex, do_not_relay: false }
+      tx_as_hex: tx_hex,
+      do_not_relay: false
     }
-    assert expected == Daemon.send_raw_transaction(tx_hex).data
+    query = Daemon.send_raw_transaction(tx_hex)
+    assert "send_raw_transaction" = query.action
+    assert expected == query.data
   end
+
+  #/start_mining
+  #/stop_mining
+  #/mining_status
+  #/save_bc
+
+  test "get_peer_list/0" do
+    assert "get_peer_list" = Daemon.get_peer_list().action
+  end
+
+  #/set_log_hash_rate
+  #/set_log_level
+  #/set_log_categories
+  #/get_transaction_pool
+  #/get_transaction_pool_hashes.bin
+  #/get_transaction_pool_stats
+  #/stop_daemon
+  #/get_limit
+  #/set_limit
+  #/out_peers
+  #/in_peers
+  #/get_outs
+  #/update
+
 end
